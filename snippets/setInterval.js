e31d251f7b5a8bc2e988037310e11d4ca8c099d9
lixiaohui <463027965@qq.com>
2020-10-15 14:55:35 +0800

    add snippets: implement setInterval using setTimeout

---
+++ snippets/setInterval.js
@@ -0,0 +1,23 @@
+class Interval {
+  constructor () {}
+  interval(fn, ms) {
+    let a = {
+      clear: () => clearTimeout(a.timer)
+    };
+    (function run() {
+      fn();
+      a.timer = setTimeout(run, ms);
+    })();
+  
+    return a;
+  }
+}
+
+export default Interval
+
+// const itl = new Interval()
+// const myInterval_1 = itl.interval(() => console.log(1), 1000); // create an "interval" 
+// const myInterval_2 = itl.interval(() => console.log(2), 1000); // create another "interval" 
+
+// // clear the first interval
+// setTimeout(() => myInterval_1.clear(), 4000)
