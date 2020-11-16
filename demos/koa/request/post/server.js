e6359565375b33d7f2549d0d3e5af6a4b907e57f
lixiaohui <463027965@qq.com>
2020-11-16 09:01:41 +0800

    docs: snippet, koa demo(koa-bodyparser)

--- demos/koa/request/post/server.js
+++ demos/koa/request/post/server.js
@@ -1,5 +1,8 @@
 const Koa = require('koa')
 const app = new Koa()
+const bodyparser = require('koa-bodyparser')
+
+app.use(bodyparser())
 
 app.use(async (ctx) => {
   // ctx.request, ctx.req, ctx.response, ctx.res, ctx.method
@@ -21,42 +24,11 @@ app.use(async (ctx) => {
     ctx.body = html
   }
   else if (ctx.url === '/' && ctx.method === 'POST') {
-    // 
-    console.log('====', ctx)
-    const postData = await parsePostData( ctx )
-    ctx.body = postData
+    ctx.body = ctx.request.body
   }
   else {
     ctx.body = '<h1>404!</h1>'
   }
 })
-
-function parsePostData(ctx) {
-  return new Promise((resolve, reject) => {
-    try {
-      let postdata = "";
-      ctx.req.addListener('data', (data) => {
-        postdata += data
-      })
-      ctx.req.addListener("end",function(){
-        let parseData = parseQueryStr( postdata )
-        resolve( parseData )
-      })
-    } catch (err) {
-      reject(err)
-    }
-  })
-}
-
-function parseQueryStr( queryStr ) {
-  let queryData = {}
-  let queryStrList = queryStr.split('&')
-  for (let [idx, queryStr ] of queryStrList.entries()) {
-    const itemList = queryStr.split('=')
-    queryData[itemList[0]] = decodeURIComponent(itemList[1])
-  }
-  return queryData
-}
-
 
 app.listen(3000, () => console.log('app is starting at 3000'))
