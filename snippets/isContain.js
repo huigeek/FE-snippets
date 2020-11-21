464d41a752de0eaff2655a7dfd02b7368442d242
lixiaohui <463027965@qq.com>
2020-11-21 11:57:53 +0800

    docs: add snippet, judge whether b contains a

---
+++ snippets/isContain.js
@@ -0,0 +1,22 @@
+// 请使用最基本的遍历来实现判断字符串 a 是否被包含在字符串 b 中，并返回第一次出现的位置（找不到返回 -1）。
+
+// a='34';b='1234567'; // 返回 2
+// a='35';b='1234567'; // 返回 -1
+// a='355';b='12354355'; // 返回 5
+// isContain(a,b);
+
+function isContain (a, b) {
+  if (b.length < a.length) return false
+  for (let i in b) {
+    if (a[0] === b[i]) {
+      let temp = true
+      for (let j in a) {
+        if (a[j] !== b[+i + +j]) {
+          temp = false
+        }
+      }
+      if (temp) return i
+    }
+  }
+  return -1
+}
