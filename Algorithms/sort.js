324bca413b81086766725c39b495acb61dce7d3e
lixiaohui <463027965@qq.com>
2020-11-23 17:21:31 +0800

    docs: add sort.js(quick sort)

--- Algorithms/sort.js
+++ Algorithms/sort.js
@@ -61,8 +61,8 @@ function merge (left, right) {
   return result
 }
 
-let temp = [13,22,1,3,53,1,53,53,5,5,6,63,23]
-console.log(mergeSort(temp))
+// let temp = [13,22,1,3,53,1,53,53,5,5,6,63,23]
+// console.log(mergeSort(temp))
 
 
 function mergeSort2(arr) {
@@ -96,6 +96,72 @@ function merge2(arr, left, right) {
   return arr
 }
 
-let arr = [3, 515, 1, 35, 66, 43]
-mergeSort2(arr)
-console.log(arr)
+// let arr = [3, 515, 1, 35, 66, 43]
+// mergeSort2(arr)
+// console.log(arr)
+
+const defaultSortingAlgorithm = (a, b) => {
+  if (a < b) return -1
+  if (a > b) return 1
+  return 0
+}
+
+const quickSort = (
+  unsortedArray,
+  sortingAlgorithm = defaultSortingAlgorithm
+) => {
+
+  // Create a sortable array to return.
+  const sortedArray = [ ...unsortedArray ];
+
+  // Recursively sort sub-arrays.
+  const recursiveSort = (start, end) => {
+
+    // If this sub-array contains less than 2 elements, it's sorted.
+    if (end - start < 1) {
+      return;
+    }
+
+    const pivotValue = sortedArray[end];
+    let splitIndex = start;
+    for (let i = start; i < end; i++) {
+      const sort = sortingAlgorithm(sortedArray[i], pivotValue);
+
+      // This value is less than the pivot value.
+      if (sort === -1) {
+
+        // If the element just to the right of the split index,
+        //   isn't this element, swap them.
+        if (splitIndex !== i) {
+          const temp = sortedArray[splitIndex];
+          sortedArray[splitIndex] = sortedArray[i];
+          sortedArray[i] = temp;
+        }
+
+        // Move the split index to the right by one,
+        //   denoting an increase in the less-than sub-array size.
+        splitIndex++;
+      }
+
+      // Leave values that are greater than or equal to
+      //   the pivot value where they are.
+    }
+
+    // Move the pivot value to between the split.
+    sortedArray[end] = sortedArray[splitIndex];
+    sortedArray[splitIndex] = pivotValue;
+
+    // Recursively sort the less-than and greater-than arrays.
+    recursiveSort(start, splitIndex - 1);
+    recursiveSort(splitIndex + 1, end);
+  };
+
+  // Sort the entire array.
+  recursiveSort(0, unsortedArray.length - 1);
+  return sortedArray;
+};
+
+
+let arr = [3, 52, 12, 5, 214, 2, 6, 7]
+console.log(quickSort(arr))
+
