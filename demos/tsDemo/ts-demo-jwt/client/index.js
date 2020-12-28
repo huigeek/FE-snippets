f71398a76ab67cb04b0ea16d08fae16a4ba02128
lixiaohui <463027965@qq.com>
2020-12-29 03:36:38 +0800

    demo: ts-demo-jwt, use express-formidable to parse form data, use jsonwebtoken to create jwt string

--- demos/tsDemo/ts-demo-jwt/client/index.js
+++ demos/tsDemo/ts-demo-jwt/client/index.js
@@ -15,6 +15,14 @@ window.onload = () => {
 
     // submit the data via XHR
     var request = new XMLHttpRequest()
+    request.onreadystatechange = function(){
+      if (request.readyState === 4 && (request.status === 200)) {
+        // 取到token后保存到sessionStorage里，之后其他接口就可以带token了
+        const res = JSON.parse(request.response)
+        !window.sessionStorage.getItem('token') && window.sessionStorage.setItem('token', res.token)
+        formElem.innerHTML = `用户名: ${res.username}; <br/>token: ${res.token}`
+      }
+    }
     // 这里请求服务端地址是server目录下开的服务器。
     // 直接请求会有跨域问题，所以server里还需要配合设置cors
     request.open("POST", "http://localhost:3000/login")
--- demos/tsDemo/ts-demo-jwt/server/index.js
+++ demos/tsDemo/ts-demo-jwt/server/index.js
@@ -1,5 +1,12 @@
 const express = require('express')
+const formidable = require('express-formidable')
+const jwt = require('jsonwebtoken')
+
 const app = express()
+
+app.use(formidable({
+  multiples: true
+}))
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*')
@@ -11,8 +18,14 @@ app.use((req, res, next) => {
   next()
 })
 
+const secret = 'ts-demo-jwt-secret'
+
 app.post('/login', (req, res) => {
-  res.send('success')
+  // 根据客户端发送的用户名和密码，生成token，返回给客户端
+  console.log('---req--', req.fields)
+  const {username, password} = req.fields
+  const token = jwt.sign({ username }, 'ts-demo-jwt-secret')
+  res.send({ username, token })
 })
 
 app.listen(3000)
