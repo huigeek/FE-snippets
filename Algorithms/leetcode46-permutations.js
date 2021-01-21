8ab4699b52c0ffb76175eec480030db974b69b1c
lixiaohui <463027965@qq.com>
2021-01-21 23:40:53 +0800

    learn algorithms(about back-track algorithm, leetcode46-permutations)

--- Algorithms/leetcode46-permutations.js
+++ Algorithms/leetcode46-permutations.js
@@ -6,7 +6,7 @@ const nums = [1, 2, 3] // 3 * 2 * 1 = 6
  * @param {number[]} nums 
  * @return {number[][]}
  */
-var permute = function(nums) {
+let permute1 = function(nums) {
   let result = []
   const backtrack = (path) => {
     if (path.length === nums.length) {
@@ -22,4 +22,25 @@ var permute = function(nums) {
   return result
 }
 
-console.log(permute(nums))
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+let permute2 = function(nums) {
+  let result = []
+  let len = nums.length
+  const backtrack = function (arr, temp) {
+    if (temp.length === len) result.push(temp)
+    else {
+      arr.forEach(v => {
+        if (temp.includes(v)) return
+        backtrack(arr, [...temp, v])
+      })
+    }
+  }
+  backtrack(nums, [])
+  return result
+}
+
+// console.log(permute1(nums))
+// console.log(permute2(nums))
